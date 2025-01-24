eb7c4e52a23dfa71d2a94a80ddfb8df7698a99c9
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2025-01-24 09:58:10 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -126,7 +126,7 @@ export default function About() {
           {[
             {
               title: 'MERN Stack Developer',
-              date: '15/02/2024 - Present',
+              date: '15/02/2024 - 15/11/2024',
               company: 'REGex Software Services',
               tasks: [
                 'Developed and maintained responsive web applications using React.',
@@ -136,24 +136,24 @@ export default function About() {
                 'Collaborated in an Agile environment for sprint planning and reviews.',
               ],
             },
-            {
-              title: 'Frontend Developer (Freelancer)',
-              date: '24/01/2023 - 24/01/2024',
-              company: 'Freelancer',
-              tasks: [
-                'Worked on frontend development projects using React.js, HTML, and CSS.',
-                'Built responsive UIs with Tailwind CSS for various clients.',
-              ],
-            },
-            {
-              title: 'Web Developer Trainee',
-              date: '10/04/2021 - 19/06/2021',
-              company: 'Internshala',
-              tasks: [
-                'Completed a 2-month training in Web Development.',
-                'Specialized in HTML, CSS, JavaScript and Bootstrap for responsive websites.',
-              ],
-            },
+            // {
+            //   title: 'Frontend Developer (Freelancer)',
+            //   date: '24/01/2023 - 24/01/2024',
+            //   company: 'Freelancer',
+            //   tasks: [
+            //     'Worked on frontend development projects using React.js, HTML, and CSS.',
+            //     'Built responsive UIs with Tailwind CSS for various clients.',
+            //   ],
+            // },
+            // {
+            //   title: 'Web Developer Trainee',
+            //   date: '10/04/2021 - 19/06/2021',
+            //   company: 'Internshala',
+            //   tasks: [
+            //     'Completed a 2-month training in Web Development.',
+            //     'Specialized in HTML, CSS, JavaScript and Bootstrap for responsive websites.',
+            //   ],
+            // },
           ].map((exp, index) => (
             <div key={index} className="bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition">
               <h3 className="text-2xl font-semibold mb-2">{exp.title}</h3>
